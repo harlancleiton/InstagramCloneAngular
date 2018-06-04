6123fd1c2d2745262bcc7cb7abf6b03bd0537b66
Harlan Cleiton <harlancleiton@gmail.com>
2018-06-04 01:10:18 -0300

    Criado alguns componentes iniciais. Instalado Bootstrap, jQuery e Popper.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
+import { BannerComponent } from './Access/banner/banner.component';
+import { LoginComponent } from './Access/login/login.component';
+import { RegisterComponent } from './Access/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    BannerComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
