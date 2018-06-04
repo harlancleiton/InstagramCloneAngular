3a71a6bb95e8768e8455c6e11d839f6162e1778f
harlancleiton <33640569+harlancleiton@users.noreply.github.com>
2018-06-04 19:00:08 -0300

    Animação do painel.

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.css']
+  styleUrls: ['./access.component.css'],
+  animations: [
+    trigger('banner-animation', [
+      state('create', style({
+        opacity: 1
+      })),
+      transition('void => create', [
+        style({
+          opacity: 0,
+          transform: 'translate(-30px, 0)'
+        }),
+        animate('500ms ease-in-out')
+      ])
+    ]),
+    trigger('panel-animation', [
+      state('create', style({
+        opacity: 1
+      })),
+      transition('void => create', [
+        style({
+          opacity: 0,
+          transform: 'translate(30px, 0)'
+        }),
+        animate('500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AccessComponent implements OnInit {
+
+  public stateCreate: string = 'create'
 
   constructor() { }
 
